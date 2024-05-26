342400fce63b8d7b63109ed2188e0896453f1cc1
KadoProG <splatrain@gmail.com>
2024-05-27 08:02:14 +0900

    add: prettierの適用

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,26 @@
-"use client";
-import React, { useEffect, useState } from "react";
+'use client';
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [message, setMessage] = useState("");
-  const [response, setResponse] = useState("");
+  const [message, setMessage] = useState('');
+  const [response, setResponse] = useState('');
   const [ws, setWs] = useState<WebSocket | null>(null);
 
   useEffect(() => {
-    const socket = new WebSocket("ws://localhost:3001/socket");
+    const socket = new WebSocket('ws://localhost:3001/socket');
 
     socket.onopen = () => {
-      console.log("Connected to WebSocket server");
+      console.log('Connected to WebSocket server');
       setWs(socket);
     };
 
     socket.onmessage = (event) => {
-      console.log("Message from WebSocket server", event.data);
+      console.log('Message from WebSocket server', event.data);
       setResponse(event.data);
     };
 
     socket.onclose = () => {
-      console.log("Disconnected from WebSocket server");
+      console.log('Disconnected from WebSocket server');
     };
 
     return () => {
@@ -31,7 +31,7 @@ export default function Home() {
   const sendMessage = React.useCallback(() => {
     if (ws && message) {
       ws.send(message);
-      setMessage("");
+      setMessage('');
     }
   }, [message, ws]);
 
